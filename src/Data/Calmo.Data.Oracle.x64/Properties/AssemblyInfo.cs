feeaf7b320b26a81f016f78931d738d1602b2190
thalesraymond <32554150+thalesraymond@users.noreply.github.com>
2017-10-05 18:49:18 -0300

    rename assembly title for nuget upload

--- src/Data/Calmo.Data.Oracle.x64/Properties/AssemblyInfo.cs
+++ src/Data/Calmo.Data.Oracle.x64/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Calmo.Data.Oracle")]
+[assembly: AssemblyTitle("Calmo.Data.Oracle.x64")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
