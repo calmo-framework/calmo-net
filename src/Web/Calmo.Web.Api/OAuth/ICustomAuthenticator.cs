ed8cc1fa3e3bba8e0eceb974ee45e73da7e52840
Raphael Nascimento <raphaelrn@gmail.com>
2018-06-25 15:12:29 -0300

    adding calmo.web.api project

---
+++ src/Web/Calmo.Web.Api/OAuth/ICustomAuthenticator.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Calmo.Web.Api.OAuth
+{
+    public interface ICustomAuthenticator
+    {
+        Task<bool> Authorize(string username, string password);
+    }
+}
