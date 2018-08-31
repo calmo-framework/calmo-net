1ed00b6ac225d5dc0d87113cf478a1fbd478884f
Raphael Nascimento <raphaelrn@gmail.com>
2018-08-31 18:07:16 -0300

    mail sender in core project

---
+++ src/Core/Calmo.Core/Net/Mail/MailSender.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net.Mail;
+
+namespace Calmo.Core.Net.Mail
+{
+    public class MailSender
+    {
+        private readonly SmtpClient _smtpClient;
+
+        public MailSender()
+        {
+            this._smtpClient = new SmtpClient();
+        }
+
+        public void Send(string toEmailAddress, string toDisplayName, string subject, string body, string ccEmailAddress = null, string ccDisplayName = null, string attachment = null)
+        {
+            if (String.IsNullOrWhiteSpace(toEmailAddress))
+                throw new ArgumentNullException("toEmailAddress");
+
+            if (String.IsNullOrWhiteSpace(toDisplayName))
+                throw new ArgumentNullException("toDisplayName");
+
+            if (String.IsNullOrWhiteSpace(subject))
+                throw new ArgumentNullException("subject");
+
+            if (String.IsNullOrWhiteSpace(body))
+                throw new ArgumentNullException("body");
+
+            var mail = new MailMessage { IsBodyHtml = true };
+
+            mail.To.Add(new MailAddress(toEmailAddress, toDisplayName));
+
+            if (!String.IsNullOrWhiteSpace(ccEmailAddress) && !String.IsNullOrWhiteSpace(ccDisplayName))
+            {
+                mail.CC.Add(new MailAddress(ccEmailAddress, ccDisplayName));
+            }
+
+            mail.Subject = subject;
+            mail.Body = body;
+
+            if (!String.IsNullOrWhiteSpace(attachment))
+            {
+                mail.Attachments.Add(new Attachment(attachment));
+            }
+
+            this._smtpClient.Send(mail);
+        }
+    }
+}
--- src/Core/Calmo.Core/Net/NetworkHelper.cs
+++ src/Core/Calmo.Core/Net/NetworkHelper.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Calmo.Net
 {
