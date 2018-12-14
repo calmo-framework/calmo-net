188f15cfd478c01309853acdd4ac1b3369f39114
Raphael Nascimento <raphaelrn@gmail.com>
2018-12-14 16:45:36 -0200

    ajustes nos pacotes de cada banco de dados

---
+++ src/Tests/Calmo.Tests/Data/DataTests2.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using Calmo.Data;
+using Calmo.Data.ActiveDirectory;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calmo.Data.SQLServer;
+
+namespace Calmo.Tests.Data
+{
+    [TestClass]
+    public class DataTests2
+    {
+        [TestMethod]
+        public void ValidateModelList()
+        {
+            var repository = new ModelRepository2();
+            var x = repository.ListSQLServer();
+        }
+    }
+
+    public class ModelRepository2 : Repository
+    {
+        public IEnumerable<Model2> ListSQLServer()
+        {
+            return this.Data.Db().UseConnection("conciliadorAlfaSeg")
+                .AsProcedure()
+                .WithParameters(new {
+                cdarquivo = 3
+            })
+                .List<Model2>("arq_pr_ConsultaArquivoPorID");
+        }
+    }
+
+    public class Model2
+    {
+        public int cdarquivo { get; set; }
+        public string descricao { get; set; }
+        public byte[] arquivo { get; set; }
+    }
+}
