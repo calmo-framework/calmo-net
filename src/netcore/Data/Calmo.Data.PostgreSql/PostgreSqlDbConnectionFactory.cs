c145661aa222f383d9f475ca0b4e3cc49d3e27c1
Raphael Nascimento <raphaelrn@gmail.com>
2021-11-01 17:39:45 -0300

    postgres implementation and .net standard 2.1 migration

---
+++ src/netcore/Data/Calmo.Data.PostgreSql/PostgreSqlDbConnectionFactory.cs
@@ -0,0 +1,17 @@
+﻿using System.Data;
+using Npgsql;
+
+namespace Calmo.Data.PostgreSql
+{
+    public class PostgreSqlDbConnectionFactory : IDbConnectionFactory
+    {
+        public IDbConnection GetDbConnection(string currentConnectionString, RepositoryDbAccess dbAccess)
+        {
+#if NETSTANDARD
+            return new NpgsqlConnection (dbAccess.GetConnectionString(currentConnectionString));
+#else
+            return new MySqlConnection(dbAccess.GetConnectionString(currentConnectionString, "MySql.Data.MySqlClient", "MySql"));
+#endif
+        }
+    }
+}
